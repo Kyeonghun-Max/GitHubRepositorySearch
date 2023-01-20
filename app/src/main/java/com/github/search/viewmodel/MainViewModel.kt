78ddd2203f45a0e7451bb2f11b887160c5a73999
KyeongHun-Max <khggang00@daum.net>
2023-01-20 15:56:13 +0900

    [Dev] 검색어 미입력, 검색결과 없음, 검색실패의 케이스 추가

--- app/src/main/java/com/github/search/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/github/search/viewmodel/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.search.R
 import com.github.search.network.model.RepositoryItem
 import com.github.search.repository.GitHubRepository
 import com.github.search.ui.adapter.RepositoryListAdapter
@@ -12,9 +13,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val repository: GitHubRepository, val adapter: RepositoryListAdapter) : ViewModel() {
+    enum class ErrorType(val stringRes: Int) {
+        NO_KEYWORD(R.string.no_keyword),
+        NO_ITEMS(R.string.no_items),
+        SEARCH_FAILED(R.string.search_failed)
+    }
 
-    val totalCount = MutableLiveData(createTotalCountTest(0))
-    val showNoResults = MutableLiveData(false)
+    val totalCount = MutableLiveData(getTotalCountText(0))
+    val showErrorText = MutableLiveData(false)
+    val errorStringRes = MutableLiveData(ErrorType.NO_ITEMS.stringRes)
     val inputKeyword = MutableLiveData("")
 
     private val _isLoading = MutableLiveData(false)
@@ -26,28 +33,40 @@ class MainViewModel @Inject constructor(private val repository: GitHubRepository
             _isLoading.postValue(false)
 
             response?.let {
-                totalCount.postValue(createTotalCountTest(it.totalCount))
+                totalCount.postValue(getTotalCountText(it.totalCount))
 
                 if (it.totalCount == 0) {
-                    showNoResults.postValue(true)
+                    showErrorText(ErrorType.NO_ITEMS)
                 } else {
-                    showNoResults.postValue(false)
+                    showErrorText.postValue(false)
                     adapter.setItems(createVms(it.items))
                 }
+            } ?: run {
+                showErrorText(ErrorType.SEARCH_FAILED)
             }
         }
     }
 
     fun onSearchBtnClicked() {
         inputKeyword.value?.let {
-            _isLoading.postValue(true)
-            repository.request(it)
+            if (it.isNotEmpty()) {
+                _isLoading.postValue(true)
+                repository.request(it)
+            } else {
+                showErrorText(ErrorType.NO_KEYWORD)
+            }
         } ?: run {
-            showNoResults.postValue(true)
+            showErrorText(ErrorType.NO_KEYWORD)
         }
     }
 
-    private fun createTotalCountTest(totalCount: Int): String {
+    private fun showErrorText(errorType: ErrorType) {
+        showErrorText.postValue(true)
+        errorStringRes.postValue(errorType.stringRes)
+        totalCount.postValue(getTotalCountText(0))
+    }
+
+    private fun getTotalCountText(totalCount: Int): String {
         return "$totalCount items"
     }
 
